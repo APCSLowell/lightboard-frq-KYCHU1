8d956eeb279e09a15a66c927438c480429401346
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-04-29 19:33:58 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -29,15 +29,15 @@ public class LightBoard
   {
     /* to be implemented in part (b) */
    int numOn = 0;
-    for(int r = 0; r < lights.length; r++){
-      if(lights[r][col]){
+    for(int row = 0; row < lights.length; row++){
+      if(lights[row][col]){
         numOn++;
       }
     }
-    if(lights[r][col] && numOn % 2 == 0){
+    if(lights[row][col] && numOn % 2 == 0){
       return false;
     }
-    if(!lights[r][col] && numOn % 3 == 0){
+    if(!lights[row][col] && numOn % 3 == 0){
       return true;
     }
     return lights[row][col];
